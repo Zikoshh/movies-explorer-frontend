caa032c96dfca3a1c46aefd65411cc0ec3dec63a
Tilek <zikoshh89@gmail.com>
2024-01-09 12:53:29 +0700

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
+import { useLocation } from 'react-router-dom';
 import './Footer.css';
+import { useEffect, useState } from 'react';
 
-const Footer = ({ isTryingToAuth }) => {
+const Footer = () => {
+  const [isToBeHidden, setIsToBeHidden] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/profile') {
+      return setIsToBeHidden(true);
+    }
+
+    if (location.pathname === '/signin') {
+      return setIsToBeHidden(true);
+    }
+
+    if (location.pathname === '/signup') {
+      return setIsToBeHidden(true);
+    }
+  }, [location])
+
   return (
-    <footer className={`footer ${isTryingToAuth ? 'footer_hidden' : ''}`}>
+    <footer className={`footer ${isToBeHidden ? 'footer_hidden' : ''}`}>
       <h5 className='footer__title'>
         Учебный проект Яндекс.Практикум х BeatFilm.
       </h5>
