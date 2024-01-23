1a7218ccadc14394622cac779e909bd736d2ae92
Tilek <zikoshh89@gmail.com>
2024-01-23 15:53:32 +0700

    fix

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,12 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/index';
+import MoviesCardList from '../MoviesCardList/index';
 
 const Movies = () => {
   return (
     <section className='movies'>
       <SearchForm />
+      <MoviesCardList />
     </section>
   );
 };
