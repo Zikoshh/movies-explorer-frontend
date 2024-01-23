241b330fda23dac630374b71052d6a026647c846
Tilek <zikoshh89@gmail.com>
2024-01-23 15:50:23 +0700

    feat
    +Компонент MoviesCardList и MoviesCard

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,3 +1,47 @@
-const MoviesCard = () => {};
+import './MoviesCard.css';
+import preview from '../../assets/images/movie-preview.jpg';
+import { useRef } from 'react';
+
+const MoviesCard = ({ isMovieSaved }) => {
+  const saveButton = useRef();
+  const savedButton = useRef();
+
+  const showSaveButton = () => {
+    saveButton.current.classList.add('card__save-button_visible');
+  };
+
+  const hideSaveButton = () => {
+    saveButton.current.classList.remove('card__save-button_visible');
+  };
+
+  const showRemoveButton = () => {
+    savedButton.current.classList.add('card__saved-button_remove');
+  };
+
+  const hideRemoveButton = () => {
+    savedButton.current.classList.remove('card__saved-button_remove');
+  };
+
+  return (
+    <article
+      className='card'
+      onMouseEnter={isMovieSaved ? showRemoveButton : showSaveButton}
+      onMouseLeave={isMovieSaved ? hideRemoveButton : hideSaveButton}
+    >
+      <img className='card__preview' src={preview} alt='#' />
+      <div className='card__info'>
+        <h4 className='card__title'>Чивоооооооооо</h4>
+        <span className='card__duration'>56ч 99м</span>
+      </div>
+      {isMovieSaved ? (
+        <button className='card__saved-button' ref={savedButton}/>
+      ) : (
+        <button className='card__save-button' ref={saveButton}>
+          Сохранить
+        </button>
+      )}
+    </article>
+  );
+};
 
 export default MoviesCard;
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,3 +1,17 @@
-const MoviesCardList = () => {};
+import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/index';
+
+const MoviesCardList = () => {
+  return (
+    <section className='cards'>
+      <MoviesCard isMovieSaved={true}/>
+      <MoviesCard isMovieSaved={true}/>
+      <MoviesCard isMovieSaved={true}/>
+      <MoviesCard isMovieSaved={false}/>
+      <MoviesCard isMovieSaved={false}/>
+      <MoviesCard isMovieSaved={false}/>
+    </section>
+  );
+};
 
 export default MoviesCardList;
