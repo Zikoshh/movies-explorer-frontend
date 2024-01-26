4910d9894146bab0130c351cb4c4b17c7a4da63f
Tilek <zikoshh89@gmail.com>
2024-01-26 15:09:01 +0700

    fix
    Фото сжималась при ширине экрана меньше 600
    Изменил название классов

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,8 +5,10 @@ const AboutMe = () => {
     <section id='about-me' className='about-me'>
       <h3 className='about-me__title'>Студент</h3>
       <div className='about-me__info'>
-        <h2 className='about-me__name'>Виталий</h2>
-        <h4 className='about-me__profession'>Фронтенд-разработчик, 30 лет</h4>
+        <h2 className='about-me__info-title'>Виталий</h2>
+        <h4 className='about-me__info-subtitle'>
+          Фронтенд-разработчик, 30 лет
+        </h4>
         <p className='about-me__desc'>
           Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня
           есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
@@ -14,8 +16,13 @@ const AboutMe = () => {
           После того, как прошёл курс по веб-разработке, начал заниматься
           фриланс-заказами и ушёл с постоянной работы.
         </p>
-        <div className='about-me__image'></div>
-        <a className='about-me__social-link' href='da'>
+        <div className='about-me__image' />
+        <a
+          className='about-me__social-link'
+          href='https://github.com/Zikoshh'
+          target='_blank'
+          rel='noreferrer'
+        >
           Github
         </a>
       </div>
