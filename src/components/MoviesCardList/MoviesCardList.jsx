74bb73bb295a38ba991fb4236d047432906d4064
Tilek <zikoshh89@gmail.com>
2024-02-08 16:07:22 +0700

    feat
    +Количество фильмов для отображения зависит от стейта displayMovieCount

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,15 +1,21 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/index';
+import { useState, useEffect } from 'react';
 
-const MoviesCardList = () => {
+const MoviesCardList = ({ movies, displayMovieCount }) => {
+  const [displayMovieList, setDisplayMovieList] = useState([]);
+
+  useEffect(() => {
+    setDisplayMovieList(() => {
+      return movies.slice(0, displayMovieCount);
+    });
+  }, [movies, displayMovieCount]);
+
   return (
     <section className='cards'>
-      <MoviesCard isMovieSaved={true}/>
-      <MoviesCard isMovieSaved={true}/>
-      <MoviesCard isMovieSaved={true}/>
-      <MoviesCard isMovieSaved={false}/>
-      <MoviesCard isMovieSaved={false}/>
-      <MoviesCard isMovieSaved={false}/>
+      {displayMovieList.map((movie) => {
+        return <MoviesCard key={movie.id} movie={movie} isMovieSaved={false} />;
+      })}
     </section>
   );
 };
