ede6b09a16edfa939597728be9d2d01a7c488a1c
Tilek <zikoshh89@gmail.com>
2024-02-08 16:08:46 +0700

    feat
    +Получаю данные фильма с верхнего компонента

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,8 +1,7 @@
 import './MoviesCard.css';
-import image from '../../assets/images/movie-image.jpg';
 import { useRef } from 'react';
 
-const MoviesCard = ({ isMovieSaved }) => {
+const MoviesCard = ({ isMovieSaved, movie }) => {
   const saveButton = useRef();
   const savedButton = useRef();
 
@@ -22,19 +21,37 @@ const MoviesCard = ({ isMovieSaved }) => {
     savedButton.current.classList.remove('card__saved-btn_remove');
   };
 
+  const getMovieDuration = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}ч ${minutes}м`;
+  };
+
   return (
     <article
       className='card'
       onMouseEnter={isMovieSaved ? showRemoveButton : showSaveButton}
       onMouseLeave={isMovieSaved ? hideRemoveButton : hideSaveButton}
     >
-      <img className='card__image' src={image} alt='test-img' />
+      <a href={movie.trailerLink} target='_blank' rel='noreferrer'>
+        <img
+          className='card__image'
+          src={`https://api.nomoreparties.co${movie.image.url}`}
+          alt={movie.nameRU}
+        />
+      </a>
       <div className='card__info'>
-        <h4 className='card__title'>Чивоооооооооо</h4>
-        <span className='card__duration'>56ч 99м</span>
+        <h4 className='card__title'>{movie.nameRU}</h4>
+        <span className='card__duration'>
+          {movie.duration < 60
+            ? `${movie.duration}м`
+            : movie.duration === 60
+            ? '1ч'
+            : getMovieDuration(movie.duration)}
+        </span>
       </div>
       {isMovieSaved ? (
-        <button className='card__saved-btn' ref={savedButton}/>
+        <button className='card__saved-btn' ref={savedButton} />
       ) : (
         <button className='card__save-btn' ref={saveButton}>
           Сохранить
